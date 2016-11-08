719053397f5ef30170ce7dc64648e168163d6dd3
Kevin McHale <k.mchale@me.com>
2016-11-08 00:50:21 -0500

    Refactored some things into methods.

--- data/populate_latlong.py
+++ data/populate_latlong.py
@@ -7,9 +7,44 @@ import binascii
 import time
 from db_cred import *
 
+def jsonRequest(url, data):
+	return json.load(urllib.urlopen(url, data))
+
+def reverseLookup(lat, lng):
+	url_prefix = ("https://maps.googleapis.com/maps/api/geocode/json?latlng=")
+	url_suffix = ("&key=" + api_key)
+	j = jsonRequest(urllib.quote(api_url_prefix + lat + ',' + lng + api_url_suffix, safe="%/:=&?~#+!$,;'@()*[]"))
+	if checkResult(j):
+		print()
+
+def checkResult(j):
+	# @refactor to switch?
+	if j['status'] == 'OVER_QUERY_LIMIT':
+		# indicates that the quota has been reached for the current google geocode API key
+		print('Over query limit (' + (used+i) + ') for key ' + api_key)
+		exit(124)
+	elif j['status'] == 'INVALID_REQUEST':
+		# generally indicates that the query (address, componentes, latlng) is missing
+		print('Invalid geocode request (url: ' + url)
+		exit(125)
+	elif j['status'] == 'ZERO_RESULTS':
+		# indicates that the geocode was successful but returned no results.
+		# -- this may occur if the geocoder was passed a non-existent address
+		print('Zero result!')
+	elif j['status'] == 'REQUEST_DENIED':
+		# indicates that the request was denied; likely a bogus API key
+		print('Geocode request denied.')
+		exit(126)
+	elif j['status'] == 'OK':
+		# indicates that no errors occured; the address was successfully parsed and at least
+		# one geocode was returned
+		return True
+	else:
+		print('Unknown response: ' + j['status'])
+	return False
+
 try:
 	# Get API key from DB
-	# @todo implement try/catch for api_connection
 	conn_api = mysql.connector.connect(host='localhost', user=rw_user, password=rw_pw, database='google_api')
 	sqlApi = ("SELECT `key` FROM api_keys WHERE name='geocode'")
 	curApi = conn_api.cursor()
@@ -31,7 +66,7 @@ try:
 
 	api_url_prefix = ("https://maps.googleapis.com/maps/api/geocode/json?region=ca&address=")
 	api_url_suffix = ("&key=" + api_key)
-	sqlGet_records = ("SELECT bus_guid, addr FROM geocode WHERE hit=0")
+	sqlGet_records = ("SELECT bus_guid, addr, lat, `long` FROM geocode WHERE hit=0")
 	sqlUpdate_coords = ("UPDATE geocode SET `lat` = %3.7f, `long` = %3.7f, hit=1 "
 			    "WHERE bus_guid = UNHEX('%s')")
 	lat = 0
@@ -70,45 +105,22 @@ try:
 		print('Fetching lat,long for address ' + row[1])
 		url = urllib.quote(api_url_prefix + row[1] + api_url_suffix, safe="%/:=&?~#+!$,;'@()*[]")
 		i = i + 1
-		j = json.load(urllib.urlopen(url, row[1]))
+		errCount = 0
+		j = jsonRequest(url, row[1])
 		sqlApi = ("UPDATE quota_counters SET `count` = `count` + 1 WHERE quota_id")
 		curApi.execute(sqlApi)
 		conn_api.commit()
 
-		if j['status'] == 'UNKNOWN_ERROR':
+		while (j['status'] == 'UNKNOWN_ERROR') & (errCount < 3):
 			# indicates that the request could not be processed due to a server error
-			# Try again? @todo
-			#j = json.load(urllib.urlopen(url, row[1]))
-			lat = 0
-			lng = 0
-		# @refactor to switch?
-		if j['status'] == 'OVER_QUERY_LIMIT':
-			# indicates that the quota has been reached for the current google geocode API key
-			print('Over query limit (' + (used+i) + ') for key ' + api_key)
-			exit(124)
-		elif j['status'] == 'INVALID_REQUEST':
-			# generally indicates that the query (address, componentes, latlng) is missing
-			print('Invalid geocode request (url: ' + url)
-			exit(125)
-		elif j['status'] == 'ZERO_RESULTS':
-			# indicates that the geocode was successful but returned no results.
-			# -- this may occur if the geocoder was passed a non-existent address
-			print('Zero result!')
-			lat = 0
-			lng = 0
-		elif j['status'] == 'REQUEST_DENIED':
-			# indicates that the request was denied; likely a bogus API key
-			print('Geocode request denied.')
-			exit(126)
-		elif j['status'] == 'OK':
-			# indicates that no errors occured; the address was successfully parsed and at least
-			# one geocode was returned
+			errCount = errCount + 1
+			j = jsonRequest(url, row[1])
+			curApi.execute(sqlApi) # increase counter again
+			conn_api.commit()
+		if checkResult(j):
 			lat = j['results'][0]['geometry']['location']['lat']
 			lng = j['results'][0]['geometry']['location']['lng']
 			print('Found!: lat=%3.7f,lng=%3.7f' % (lat, lng))
-		else:
-			print('Unknown response: ' + j['status'])
-
 		# Execute update on row
 		conn_put.cursor().execute(sqlUpdate_coords % (lat, lng, guid.hex))
 		conn_put.commit()
@@ -120,3 +132,5 @@ else:
 	api_conn.close()
 
 exit(0)
+
+
